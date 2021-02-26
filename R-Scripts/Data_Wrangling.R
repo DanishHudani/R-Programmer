726adfd44830b9fd658acc3a3b12bcbcb4a00d06
DanishHudani <danish.hudani78@gmail.com>
2021-02-27 02:03:34 +0500

    Using Filter with pipe pperator (%>%) to analyze the subset of observations.

--- R-Scripts/Data_Wrangling.R
+++ R-Scripts/Data_Wrangling.R
@@ -6,3 +6,9 @@ library(dplyr)
 
 # Look at the gapminder dataset
 gapminder
+
+# Filter the gapminder dataset for the year 1957
+gapminder %>% filter(year == 1957)
+
+# Filter for China in 2002
+gapminder %>% filter(country == "China", year == 2002)
