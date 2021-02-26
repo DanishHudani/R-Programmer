38eedd1d08f7abba8179f7253e17e123ac4f30d3
DanishHudani <danish.hudani78@gmail.com>
2021-02-27 03:38:31 +0500

    Implemented Faceting on continent and year

--- R-Scripts/Data_Visualization.R
+++ R-Scripts/Data_Visualization.R
@@ -20,8 +20,20 @@ ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) + geom_point()
 + scale_x_log10() + scale_y_log10()
 
 # Scatter plot comparing pop and lifeExp, with color representing continent
-ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent)) + geom_point() + scale_x_log10()
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent)) + 
+  geom_point() + scale_x_log10()
 
+# Add the size aesthetic to represent a country's gdpPercap
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent, size = gdpPercap)) +
+  geom_point() +
+  scale_x_log10()
 
+# Scatter plot comparing pop and lifeExp, faceted by continent
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + 
+  geom_point() + scale_x_log10() + facet_wrap(~ continent)
 
+# Scatter plot comparing gdpPercap and lifeExp, with color representing continent
+# and size representing population, faceted by year
+ggplot(gapminder, aes(x = gdpPercap, y = lifeExp, color = continent, size = pop)) + 
+  geom_point() + scale_x_log10() + facet_wrap(~ year)
 
