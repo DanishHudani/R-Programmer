7ccdd5dd6701a7e16bf8c4ea264daab5283b983e
DanishHudani <danish.hudani78@gmail.com>
2021-02-27 02:12:40 +0500

    Used arrange() for data wrangling

--- R-Scripts/Data_Wrangling.R
+++ R-Scripts/Data_Wrangling.R
@@ -12,3 +12,24 @@ gapminder %>% filter(year == 1957)
 
 # Filter for China in 2002
 gapminder %>% filter(country == "China", year == 2002)
+
+# Sort in ascending order of lifeExp
+gapminder %>% arrange(lifeExp)
+
+
+# Sort in descending order of lifeExp
+gapminder %>% arrange(desc(lifeExp))
+
+# Filter for the year 1957, then arrange in descending order of population
+gapminder %>% filter(year == 1957) %>% arrange(desc(pop))
+
+
+
+
+
+
+
+
+
+
+
