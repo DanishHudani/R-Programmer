3876f3ac61c5f7e237681366b88a50916926705a
DanishHudani <danish.hudani78@gmail.com>
2021-02-27 03:09:52 +0500

    Implemented log scale and additional aesthetics on gapminder_1952

--- R-Scripts/Data_Visualization.R
+++ R-Scripts/Data_Visualization.R
@@ -12,3 +12,16 @@ ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) + geom_point()
 # Create a scatter plot with pop on the x-axis and lifeExp on the y-axis
 ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + geom_point()
 
+# Change this plot to put the x-axis on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + geom_point() + scale_x_log10()
+
+# Scatter plot comparing pop and gdpPercap, with both axes on a log scale
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) + geom_point()
++ scale_x_log10() + scale_y_log10()
+
+# Scatter plot comparing pop and lifeExp, with color representing continent
+ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent)) + geom_point() + scale_x_log10()
+
+
+
+
