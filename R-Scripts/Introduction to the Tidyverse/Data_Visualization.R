a0ccd5cb9fcfac05ab9909167fc8aa6ac186b9e5
DanishHudani <danish.hudani78@gmail.com>
2021-02-28 00:33:43 +0500

    Error on line 20 resolved by correcting scale_x_log10()

--- R-Scripts/Introduction to the Tidyverse/Data_Visualization.R
+++ R-Scripts/Introduction to the Tidyverse/Data_Visualization.R
@@ -16,8 +16,8 @@ ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + geom_point()
 ggplot(gapminder_1952, aes(x = pop, y = lifeExp)) + geom_point() + scale_x_log10()
 
 # Scatter plot comparing pop and gdpPercap, with both axes on a log scale
-ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) + geom_point()
-+ scale_x_log10() + scale_y_log10()
+ggplot(gapminder_1952, aes(x = pop, y = gdpPercap)) + geom_point() + 
+  scale_x_log10() + scale_y_log10()
 
 # Scatter plot comparing pop and lifeExp, with color representing continent
 ggplot(gapminder_1952, aes(x = pop, y = lifeExp, color = continent)) + 
