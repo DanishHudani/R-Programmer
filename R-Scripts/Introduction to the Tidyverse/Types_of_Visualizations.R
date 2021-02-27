7f75a3a48d8bc64ece11e3a4fbc69b05668dc1cd
DanishHudani <danish.hudani78@gmail.com>
2021-02-28 02:44:57 +0500

    Used Visualization: Bar on gapminder dataset

--- R-Scripts/Introduction to the Tidyverse/Types_of_Visualizations.R
+++ R-Scripts/Introduction to the Tidyverse/Types_of_Visualizations.R
@@ -16,3 +16,20 @@ by_year_continent <- gapminder %>% group_by(year, continent) %>%
 # Create a line plot showing the change in medianGdpPercap by continent over time
 ggplot(by_year_continent, aes(x = year, y = medianGdpPercap, color = continent)) + 
   geom_line() + expand_limits(y = 0)
+
+# Summarize the median gdpPercap by continent in 1952
+by_continent <- gapminder %>% filter(year == 1952) %>% group_by(continent) %>%
+  summarize(medianGdpPercap = median(gdpPercap))
+
+# Create a bar plot showing medianGdp by continent
+ggplot(by_continent, aes(x = continent, y = medianGdpPercap)) + geom_col()
+
+# Filter for observations in the Oceania continent in 1952
+oceania_1952 <- gapminder %>% filter(continent == "Oceania", year == 1952)
+
+# Create a bar plot of gdpPercap by country
+ggplot(oceania_1952, aes(x = country, y = gdpPercap)) + geom_col()
+
+
+
+
