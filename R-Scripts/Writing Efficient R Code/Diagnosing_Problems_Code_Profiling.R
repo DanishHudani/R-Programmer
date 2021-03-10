c5559562c84f7e3d757cd096693bc766b3bbfcf7
DanishHudani <danish.hudani78@gmail.com>
2021-03-11 00:51:05 +0500

    Used profvis and microbenchmark package for code profiling to identify bottleneck

---
+++ R-Scripts/Writing Efficient R Code/Diagnosing_Problems_Code_Profiling.R
@@ -0,0 +1,89 @@
+library(ggplot2)
+
+movies <- read.csv("datasets/movies.csv", header = FALSE)
+
+# Load the data set
+data(movies, package = "ggplot2movies") 
+
+# Load the profvis package
+library("profvis")
+
+# Profile the following code with the profvis function
+profvis({
+  # Load and select data
+  comedies <- movies[movies$Comedy == 1, ]
+  
+  # Plot data of interest
+  plot(comedies$year, comedies$rating)
+  
+  # Loess regression line
+  model <- loess(rating ~ year, data = comedies)
+  j <- order(comedies$year)
+  
+  # Add fitted line to the plot
+  lines(comedies$year[j], model$fitted[j], col = "red")
+})   ## Remember the closing brackets!
+
+
+# Load the microbenchmark package
+library(microbenchmark)
+
+# The previous data frame solution is defined
+# d() Simulates 6 dices rolls
+d <- function() {
+  data.frame(
+    d1 = sample(1:6, 3, replace = TRUE),
+    d2 = sample(1:6, 3, replace = TRUE)
+  )
+}
+
+# Complete the matrix solution
+m <- function() {
+  matrix(sample(1:6, 6, replace = TRUE), ncol = 2)
+}
+
+# Use microbenchmark to time m() and d()
+microbenchmark(
+  data.frame_solution = d(),
+  matrix_solution     = m()
+)
+
+rolls <- matrix(c(6, 2, 4, 3, 6, 2), ncol = 2, byrow = TRUE)
+
+# Define the previous solution 
+app <- function(x) {
+  apply(x, 1, sum)
+}
+
+# Define the new solution
+r_sum <- function(x) {
+  rowSums(x)
+}
+
+# Compare the methods
+microbenchmark(
+  app_sol = app(rolls),
+  r_sum_sol = r_sum(rolls)
+)
+
+is_double <- c(FALSE, TRUE, TRUE)
+
+
+# Define the previous solution
+move <- function(is_double) {
+  if (is_double[1] & is_double[2] & is_double[3]) {
+    current <- 11 # Go To Jail
+  }
+}
+
+# Define the improved solution
+improved_move <- function(is_double) {
+  if (is_double[1] && is_double[2] && is_double[3]) {
+    current <- 11 # Go To Jail
+  }
+}
+
+# microbenchmark both solutions
+# Very occassionally the improved solution is actually a little slower
+# This is just random chance
+microbenchmark(move, improved_move, times = 1e5)
