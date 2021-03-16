776dff663afd4cc6349a21ce3a7e561e23551b19
DanishHudani <danish.hudani78@gmail.com>
2021-03-16 13:22:54 +0500

    Working with stringr package: str_c(), str_lenght(), str_sub()

---
+++ R-Scripts/String Manipulation with stringr in R/Introduction_to_stringr.R
@@ -0,0 +1,75 @@
+
+library(stringr)
+
+my_toppings <- c("cheese", NA, NA)
+my_toppings_and <- paste(c("", "", "and "), my_toppings, sep = "")
+
+# Print my_toppings_and
+my_toppings_and
+
+# Use str_c() instead of paste(): my_toppings_str
+my_toppings_str <- str_c(c("", "", "and "), my_toppings)
+
+# Print my_toppings_str
+my_toppings_str
+
+# paste() my_toppings_and with collapse = ", "
+paste(my_toppings_and, collapse = ", ")
+
+# str_c() my_toppings_str with collapse = ", "
+str_c(my_toppings_str, collapse = ", ")
+
+
+
+library(babynames)
+library(dplyr)
+
+
+
+# Extracting vectors for boys' and girls' names
+babynames_2014 <- filter(babynames, year == 2014)
+boy_names <- filter(babynames_2014, sex == "M")$name
+girl_names <- filter(babynames_2014, sex == "F")$name
+
+# Take a look at a few boy_names
+head(boy_names)
+
+# Find the length of all boy_names
+boy_length <- str_length(boy_names)
+
+# Take a look at a few lengths
+head(boy_length)
+
+# Find the length of all girl_names
+girl_length <- str_length(girl_names)
+
+# Find the difference in mean length
+mean(girl_length) - mean(boy_length)
+
+# Confirm str_length() works with factors
+head(str_length(factor(boy_names)))
+
+
+
+# Extract first letter from boy_names
+boy_first_letter <- str_sub(boy_names, 1, 1)
+
+# Tabulate occurrences of boy_first_letter
+table(boy_first_letter)
+
+# Extract the last letter in boy_names, then tabulate
+boy_last_letter <- str_sub(boy_names, -1, -1)
+table(boy_last_letter)
+
+# Extract the first letter in girl_names, then tabulate
+girl_first_letter <- str_sub(girl_names, 1, 1)
+table(girl_first_letter)
+
+# Extract the last letter in girl_names, then tabulate
+girl_last_letter <- str_sub(girl_names, -1, -1)
+table(girl_last_letter)
+
+
+
+
+
